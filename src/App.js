94e2b57a84c737daeeb4eb8ba1053ee77e56fd14
Juan Felipe <juanmontana1398@gmail.com>
2020-07-04 12:41:59 -0500

    Usando @media con la libreria radium. cap 5 - video 70

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
     persons: [
-      {id: 'k1' ,name: 'Max', age: 28 },
-      {id: 'k2' ,name: 'Manu', age: 29 },
-      {id: 'k3' ,name: 'Stephanie', age: 26 }
+      { id: 'k1', name: 'Max', age: 28 },
+      { id: 'k2', name: 'Manu', age: 29 },
+      { id: 'k3', name: 'Stephanie', age: 26 },
     ],
     otherState: 'some other value',
-    showPersons: false 
-  }
+    showPersons: false,
+  };
 
   switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState( {
+    this.setState({
       persons: [
-        {name: newName, age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    } )
-  }
+        { name: 'Stephanie', age: 27 },
+      ],
+    });
+  };
 
-  nameChangedHandler = (event,person_id) => {
+  nameChangedHandler = (event, person_id) => {
     console.log(1);
     //Buscamos el index de la persona a editar
-    const personIndex = this.state.persons.findIndex(person => {
+    const personIndex = this.state.persons.findIndex((person) => {
       return person.id === person_id;
-    })
+    });
     //Creamos un objeto con los datos de esa persona
     const person = {
-      ...this.state.persons[personIndex]
-    }
-    //Le asignamos el nuevo valor al objeto temporal 
+      ...this.state.persons[personIndex],
+    };
+    //Le asignamos el nuevo valor al objeto temporal
     person.name = event.target.value;
     //Creamos una copia de la lista de personas
     const persons = [...this.state.persons];
@@ -44,31 +44,30 @@ class App extends Component {
     persons[personIndex] = person;
     //Actualizamos el estado con el valor de la persona ya editada
     this.setState({
-      persons: persons
-    })
-  }
-
+      persons: persons,
+    });
+  };
 
   //Handler para mostrar las personas y ocultarlas
   showPersonsHandler = () => {
     const actual_state = this.state.showPersons;
-    this.setState ({
-     showPersons: !actual_state 
+    this.setState({
+      showPersons: !actual_state,
     });
-  }
+  };
 
   //Handler para eliminar un item de la lista personas
   deletePersonHandler = (index) => {
-    //Bad practice 
+    //Bad practice
     //const persons = this.state.persons;
     //good practice
     const persons = [...this.state.persons];
-    persons.splice(index,1);
+    persons.splice(index, 1);
     this.setState({
-      persons: persons
-    })
-  }
-  render () {
+      persons: persons,
+    });
+  };
+  render() {
     /**
      * Segunda manera de condicionar y hacer el contenido dinamico.
      */
@@ -84,59 +83,66 @@ class App extends Component {
       // de la siguiente manera.
       ':hover': {
         backgroundColor: 'yellow',
-        color: 'black'
-      }
-    }
+        color: 'black',
+      },
+    };
     //Cambio de estilos dinamicamente con clases.
     const classes = [];
     let persons = null;
-    if (this.state.showPersons){
+    if (this.state.showPersons) {
       persons = (
         <div>
           {
             /**
-            * Renderizando dinamicamente el contenido del arreglo. 
-            */
-            this.state.persons.map( (person,index) => {
-              return <Person
-                click= {()=> this.deletePersonHandler(index)}
-                name= {person.name}
-                age= {person.age}
-                key= {person.id}
-                changed={(event) => this.nameChangedHandler(event,person.id)}
+             * Renderizando dinamicamente el contenido del arreglo.
+             */
+            this.state.persons.map((person, index) => {
+              return (
+                <Person
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name}
+                  age={person.age}
+                  key={person.id}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}
                 />
+              );
             })
           }
         </div>
-      )
-        //Cambiamos el valor del objeto showPersonStyles, con la finalidad de que el boton se muestre
-        //rojo y verde dependiendo si se van a mostrar u ocultar las personas
-        showPersonStyles.backgroundColor ="red";
-        //Cambiamos el estilo del pseudo selector, como es un string tenemos que poner ['pseudo_selector'] para acceder a el
-        showPersonStyles[':hover'] = {
-          backgroundColor: 'blue',
-          color: 'white',
-        }
+      );
+      //Cambiamos el valor del objeto showPersonStyles, con la finalidad de que el boton se muestre
+      //rojo y verde dependiendo si se van a mostrar u ocultar las personas
+      showPersonStyles.backgroundColor = 'red';
+      //Cambiamos el estilo del pseudo selector, como es un string tenemos que poner ['pseudo_selector'] para acceder a el
+      showPersonStyles[':hover'] = {
+        backgroundColor: 'blue',
+        color: 'white',
+      };
     }
 
     //Dependiendo de cuantas personas hay se le insertan las clases al arreglo classes.
-    if(this.state.persons.length <= 2){
+    if (this.state.persons.length <= 2) {
       classes.push('red');
     }
-    if(this.state.persons.length <= 1){
+    if (this.state.persons.length <= 1) {
       classes.push('bold');
     }
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App Udemy Course</h1>
-        {/* Se hace un join al arreglo clases para que quede asi -> clase1 clase2 */}
-        <p className={classes.join(" ")}>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
-        <button style={showPersonStyles} onClick={this.showPersonsHandler}>Show Persons</button>
-        {
-          //Segunda manera de condicionar el contenido
-          persons
-          /** 
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App Udemy Course</h1>
+          {/* Se hace un join al arreglo clases para que quede asi -> clase1 clase2 */}
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button onClick={() => this.switchNameHandler('Maximilian!!')}>
+            Switch Name
+          </button>
+          <button style={showPersonStyles} onClick={this.showPersonsHandler}>
+            Show Persons
+          </button>
+          {
+            //Segunda manera de condicionar el contenido
+            persons
+            /** 
            *Condicion que verifica si se deben o no mostrar las personas
            *Tipo de condiciones 1-> usando expresiones terninarias 
           this.state.showPersons ?
@@ -155,8 +161,9 @@ class App extends Component {
             </div>
           :null
           */
-        }
-      </div>
+          }
+        </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Radium from 'radium';
 //Los css se deben importar donde se van a utilizar
 import './Person.css';
 const person = ( props ) => {
+    //Usando media querys con Radium
+    const personStyle = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    }
     return (
-        <div className="person">
+        <div className="person" style={personStyle}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
@@ -11,4 +18,4 @@ const person = ( props ) => {
     )
 };
 
-export default person;
+export default Radium(person);
