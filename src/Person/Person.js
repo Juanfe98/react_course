96744770b772b0a7cf4c4a9e8bc031c5e7f94989
Juan Felipe <juanmontana1398@gmail.com>
2020-07-04 14:34:54 -0500

    Ajuste y solucion de errores por ESLINT

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,20 +2,22 @@ import React from 'react';
 import Radium from 'radium';
 //Los css se deben importar donde se van a utilizar
 import './Person.css';
-const person = ( props ) => {
-    //Usando media querys con Radium
-    const personStyle = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    }
-    return (
-        <div className="person" style={personStyle}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </div>
-    )
+const person = (props) => {
+  //Usando media querys con Radium
+  const personStyle = {
+    '@media (min-width: 500px)': {
+      width: '450px',
+    },
+  };
+  return (
+    <div className="person" style={personStyle}>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
 };
 
 export default Radium(person);
